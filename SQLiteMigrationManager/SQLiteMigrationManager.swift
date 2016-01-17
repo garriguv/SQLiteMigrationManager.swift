03537e1a0469b70ffe63778887cd6aac921e7881
Vincent Garrigues <vincent.garrigues@gmail.com>
2016-01-17 21:00:09 +0100

    currentVersion()

--- SQLiteMigrationManager/SQLiteMigrationManager.swift
+++ SQLiteMigrationManager/SQLiteMigrationManager.swift
@@ -5,6 +5,7 @@ public struct SQLiteMigrationManager {
   let db: Connection
 
   private let schemaMigrations = Table("schema_migrations")
+  private let version = Expression<Int64>("version")
 
   public init(_ db: Connection) {
     self.db = db
@@ -16,5 +17,13 @@ public struct SQLiteMigrationManager {
     let name = Expression<String>("name")
     let count = db.scalar(sqliteMaster.filter(type == "table" && name == "schema_migrations").count)
     return count == 1;
+  }
+
+  public func currentVersion() -> Int64 {
+    if !hasMigrationsTable() {
+      return 0
+    }
+
+    return db.scalar(schemaMigrations.select(version.max)) ?? 0
   }
 }
--- SQLiteMigrationManagerTests/SQLiteMigrationManagerSpec.swift
+++ SQLiteMigrationManagerTests/SQLiteMigrationManagerSpec.swift
@@ -14,7 +14,7 @@ class SQLiteMigrationManagerSpec: QuickSpec {
       subject = SQLiteMigrationManager(db)
     }
 
-    describe("hasMigrationsTable") {
+    describe("hasMigrationsTable()") {
       context("when there is no migration table") {
         it("returns false") {
           expect(subject.hasMigrationsTable()).to(beFalse())
@@ -23,7 +23,7 @@ class SQLiteMigrationManagerSpec: QuickSpec {
 
       context("when there is a migration table") {
         beforeEach {
-          try! db.execute("CREATE TABLE schema_migrations(version INTEGER UNIQUE NOT NULL);")
+          createMigrationTable(db)
         }
 
         it("returns true") {
@@ -31,5 +31,44 @@ class SQLiteMigrationManagerSpec: QuickSpec {
         }
       }
     }
+
+    describe("currentVersion()") {
+      context("when there is no migration table") {
+        it("returns 0") {
+          expect(subject.currentVersion()).to(equal(0))
+        }
+      }
+
+      context("when there is a migration table") {
+        beforeEach {
+          createMigrationTable(db)
+        }
+
+        context("when the migration table is empty") {
+          it("returns 0") {
+            expect(subject.currentVersion()).to(equal(0))
+          }
+        }
+
+        context("when the migration table cointains migrations") {
+          beforeEach {
+            insertMigration(db, version: 1337)
+          }
+
+          it("returns the latest migration") {
+            expect(subject.currentVersion()).to(equal(1337))
+          }
+        }
+      }
+    }
   }
+}
+
+private func createMigrationTable(db: Connection) {
+  try! db.execute("CREATE TABLE schema_migrations(version INTEGER UNIQUE NOT NULL);")
+}
+
+private func insertMigration(db: Connection, version: Int64) {
+  let stmt = try! db.prepare("INSERT INTO schema_migrations(version) VALUES (?);")
+  try! stmt.run(version)
 }
