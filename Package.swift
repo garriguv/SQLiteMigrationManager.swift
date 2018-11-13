ff853fc728eca05f46ac5a0d5f9ef9d6d8b19bb7
Vincent Garrigues <vincent.garrigues@gmail.com>
2018-11-13 17:21:40 +0100

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "SQLiteMigrationManager.swift",
+    products: [
+        .library(name: "SQLiteMigrationManager", targets: ["SQLiteMigrationManager"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/stephencelis/SQLite.swift.git", from: "0.11.5")
+    ],
+    targets: [
+        .target(
+            name: "SQLiteMigrationManager",
+            dependencies: ["SQLite"],
+            path: "SQLiteMigrationManager",
+            sources: ["SQLiteMigrationManager.swift"]),
+        .testTarget(
+            name: "SQLiteMigrationManagerTests",
+            dependencies: ["SQLiteMigrationManager"],
+            path: "SQLiteMigrationManagerTests",
+            sources: ["SQLiteMigrationManagerTests.swift"])
+    ])
